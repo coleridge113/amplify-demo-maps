305d6658b953df9cd5cc5f2d65119f8f24ee11d3
Jose Luna <josemanuelmluna@gmail.com>
2025-12-26 21:11:56 +0800

    updated to show map marker on map

---
+++ aws-cli.md
@@ -0,0 +1,8 @@
+aws location get-device-position-history \
+  --tracker-name MetromartDemoTracker \
+  --device-id Vehicle-1 \
+  --region ap-northeast-1 \
+  --max-results 10
+
+
+13.9736382,121.6761132
--- src/component/MapView.jsx
+++ src/component/MapView.jsx
@@ -1,6 +1,54 @@
 import React, { useEffect, useRef } from "react";
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
+import { fromCognitoIdentityPool } from "@aws-sdk/credential-providers"; 
+import { LocationClient, GetDevicePositionCommand, BatchUpdateDevicePositionCommand } from "@aws-sdk/client-location";
+
+const awsRegion = import.meta.env.VITE_AWS_REGION; 
+const cognitoIdentityPoolId = import.meta.env.VITE_AWS_COGNITO_IDENTITY_POOL_ID
+const trackerName = "MetromartDemoTracker"
+const deviceId = "Vehicle-1"
+const hardware = {
+    latitude: 13.9735689,
+    longitude: 121.6787518
+}
+
+const client = new LocationClient({
+    region: awsRegion,
+    credentials: fromCognitoIdentityPool({
+        clientConfig: { region: awsRegion},
+        identityPoolId: cognitoIdentityPoolId
+    })
+});
+
+async function updatePositions() {
+    const command = new BatchUpdateDevicePositionCommand({
+        TrackerName: "MetromartDemoTracker",
+        Updates: [
+            {
+                DeviceId: deviceId,
+                Position: [ hardware.longitude, hardware.latitude ],
+                SampleTime: new Date().toISOString(),
+                PositionProperties: { name: "Vehicle-1" },
+                Accuracy: { Horizontal: 10 }
+            }
+        ]
+    });
+
+    const response = await client.send(command);
+    console.log("Update response:", response);
+}
+
+async function fetchPosition() {
+    try {
+        const command = new GetDevicePositionCommand({ TrackerName: trackerName, DeviceId: deviceId });
+        const response = await client.send(command);
+        return response.Position;
+    } catch (err) {
+        console.error("Error fetching position:", err);
+        return null;
+    }
+}
 
 const MapView = () => {
     const mapContainer = useRef(null);
@@ -12,12 +60,21 @@ const MapView = () => {
         map.current = new maplibregl.Map({
             container: mapContainer.current,
             style: "https://tiles.openfreemap.org/styles/liberty", // demo style
-            center: [120.9, 13.9], // Pagbilao, Philippines as example
-            zoom: 10,
+            center: [121.6761132, 13.9736382], // Pagbilao, Philippines as example
+            zoom: 16,
         });
 
-        // Add navigation controls
         map.current.addControl(new maplibregl.NavigationControl(), "top-right");
+
+        async function addTrackerMarker() {
+            await updatePositions()
+            const pos = await fetchPosition();
+            new maplibregl.Marker({ color: "red" })
+                .setLngLat(pos)
+                .addTo(map.current);
+        }
+
+        addTrackerMarker();
     }, []);
 
     return (
